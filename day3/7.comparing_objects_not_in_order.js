5b68e53755f24a8c220c60ad8b5812fdcd632dcb
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-09-13 09:57:31 +0530

    Add files via upload

---
+++ day3/7.comparing_objects_not_in_order.js
@@ -0,0 +1,34 @@
+
+//How to compare two JSON have the same properties without order?
+
+
+var obj1 = {"name":"GUVI","class":"FS"};
+var obj2 = {"class":"FS","name":"GUVI"};
+
+
+function noOrder(obj1, obj2) {
+    const obj1Keys = Object.keys(obj1);
+    const obj2Keys = Object.keys(obj2);
+
+    if(obj1Keys.length !== obj2Keys.length) {
+        return false;
+    }
+
+    for (let objKey of obj1Keys) {
+        if (obj1[objKey] !== obj2[objKey]) {
+            if(typeof obj1[objKey] == "object" && typeof obj2[objKey] == "object") {
+                if(!isEqual(obj1[objKey], obj2[objKey])) {
+                    return false;
+                }
+            } 
+            else {
+                return false;
+            }
+        }
+    }
+
+    return true;
+}
+console.log(noOrder(obj1,obj2));
+
+//output:true
