7d8f7b67b4b04df2479954cd9db367138e6e8a1c
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-09-14 18:26:12 +0530

    Update 1.taskforCallBack.js

--- day4/1.taskforCallBack.js
+++ day4/1.taskforCallBack.js
@@ -9,21 +9,23 @@ Function array
 */
 
 function process(a,b) {
+    //this function returns the desired operation of 2 parameters to the user
 	var val1 = 200, val2= 800;
     return a[b-1](val1,val2);
 
 }
 
 function add(a,b)
-{
+{//this function returns addition of 2 parameters
 	return a+b
 }
 function sub(a,b)
-{
+{//this function returns subtraction of 2 parameters
 	return a-b
 }
+
 function mul(a,b)
-{
+{//this function returns multiplication of 2 parameters
 	return a*b
 }
 
@@ -31,4 +33,10 @@ var opers = [add,sub,mul];
 var oper1 = process(opers,1);
 var oper2 = process(opers,2);
 var oper3 = process(opers,3);
-console.log(oper1,oper2,oper3);
+
+try{
+    console.log(oper1,oper2,oper3);
+}catch(err){
+    console.log("Error Occured")
+}
+
