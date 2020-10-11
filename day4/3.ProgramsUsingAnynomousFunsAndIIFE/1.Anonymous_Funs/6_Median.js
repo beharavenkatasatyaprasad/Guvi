c88993ead70b5577003d3f982b3550a0225f75d3
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 12:34:26 +0530

    Update 6_Median.js

--- day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/6_Median.js
+++ day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/6_Median.js
@@ -1,15 +1,15 @@
 /*
 Return median of two sorted arrays of same size 
 */
-var MedianOf = function (array1,array2) { 
-var m1 = GetMid(array1);
-var m2 = GetMid(array2);
-var median = (m1+m2)/2;
+let MedianOf = function (array1,array2) { 
+let m1 = GetMid(array1);
+let m2 = GetMid(array2);
+let median = (m1+m2)/2;
 console.log(median);
 
 function GetMid(array){
-var arr_len = array.length 
-mid = arr_len/2;
+let arr_len = array.length 
+let mid = arr_len/2;
 if(mid !== parseInt(mid)){
 mid = mid-0.5;
 }else{
@@ -18,9 +18,9 @@ mid = mid-0.5;
 return array[mid];
 }
 }
-var array1 = [1, 12, 15, 26, 38];
-var array2 = [2, 13, 17, 30, 45]
-MedianOf(array1,array2);
+let arr1 = [1, 12, 15, 26, 38];
+let arr2 = [2, 13, 17, 30, 45]
+MedianOf(arr1,arr2);
 
 
 /*output : 
