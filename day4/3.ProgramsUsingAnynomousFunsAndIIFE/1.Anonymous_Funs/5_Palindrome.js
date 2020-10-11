35e27edeca3855de35600c9d78aeb53de60e7183
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 11:46:18 +0530

    Update 5_Palindrome.js

--- day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/5_Palindrome.js
+++ day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/5_Palindrome.js
@@ -1,7 +1,7 @@
 /*
 Return all the palindromes in an array
 */
-var palindrome = function (array) { 
+var Palindrome = function (array) { 
 function ChangeArrToStr(array){
 	//This function will parse all elements in array to Strings and push them into a new array
     
@@ -31,11 +31,11 @@ function isPalindrome(n)
    var split_ele=n.split("");//splits the elements into an array
     var rev = reverse(split_ele);//reversed element is stored in rev
    
+	//this function will reverse the splited array
     function reverse(split_element){
-        //this function will reverse the splited array
         var revarr = [];
         for(var i=split_element.length-1;i>=0;i--){
-            revarr.push(split_element[i])
+            revarr.push(split_element[i]);
         }
         return revarr;
     }
@@ -59,7 +59,7 @@ for(var i=0;i<strarray.length;i++){
 }
 }
 var array = [121,213,332,454,"ada",671,767,"sas"];
-palindrome(array)
+Palindrome(array);
 
 /*output : 
 121
