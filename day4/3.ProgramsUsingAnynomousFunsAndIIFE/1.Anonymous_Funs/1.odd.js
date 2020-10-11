24a435f08d04c5090d159fb0d972f5de14ed3474
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 12:36:38 +0530

    Update 1.odd.js

--- day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/1.odd.js
+++ day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/1.odd.js
@@ -1,29 +1,26 @@
 /*
 Problem:Print odd numbers in an array 
 */
-var PrintOdd = function(array) { 
+lat PrintOdd = function(array) { 
     try{
 		if(typeof(array)!=='object') throw "Input is not an array"
 		else console.log(oddArray(array));
 	}catch(err){
 		console.log(err);
 	}
-	
-         
-		function oddArray(array){
-			//this function takes an array and returns a Newarray contaning odd elements from input array
-			var oddarray=[];
-			for(var i=0;i<array.length;i++){
+	function oddArray(array){//this function takes an array and returns a Newarray contaning odd elements from input array
+			let oddarray=[];
+			for(let i=0;i<array.length;i++){
 				if(array[i]%2!=0){
-				oddarray.push(array[i])
+					oddarray.push(array[i]);
 				}
 			} return oddarray;
 		}
 		
 }
-var array = [1,2,3,4,5,6,7,8,9]
-var notarray = "satya"
+let arr = [1,2,3,4,5,6,7,8,9]
+let notarr = "prasad"
 
-PrintOdd(array);//[ 1, 3, 5, 7, 9 ]
+PrintOdd(arr);//[ 1, 3, 5, 7, 9 ]
 
-PrintOdd(notarray);//Input is not an array
+PrintOdd(notarr);//Input is not an array
