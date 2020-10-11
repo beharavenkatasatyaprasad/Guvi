f2fd528be64561049553850cf951249196b200b9
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 12:22:39 +0530

    Update 5_Palindrome.js

--- day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/5_Palindrome.js
+++ day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/5_Palindrome.js
@@ -1,46 +1,36 @@
 /*
 Return all the palindromes in an array
 */
-var Palindrome = function (array) { 
-function ChangeArrToStr(array){
-	//This function will parse all elements in array to Strings and push them into a new array
-    
-	var strarr = [];//new array
-	var c = "";
-	
-    for(var i=0;i<array.length;i++){
+let Palindrome = function (array) { 
+function ChangeArrToStr(array){	//This function will parse all elements in array to Strings and push them into a new array\
+	let strarr = [];//new array
+	let c = "";
+    for(let i=0;i<array.length;i++){
         if(isNaN(array[i])){
             c = array[i];
         }
-    else{
-        //if the element is not string change it into a string and store in variable c
-        
-        c = c + array[i];
-       
-    }
+    else{//if the element is not string change it into a string and store in variable c
+        c = c + array[i];       
+  	  }
     strarr.push(c);//push parsed thing into new array
-        
     }
-    
    return (strarr);//return the newarray
 }
-var strarray = ChangeArrToStr(array);//parsedarray is stored in strarray
+let strarray = ChangeArrToStr(array);//parsedarray is stored in strarray
 function isPalindrome(n)
-{   
-    //This function will check wether the elements in strarray is palindrome or not
-   var split_ele=n.split("");//splits the elements into an array
-    var rev = reverse(split_ele);//reversed element is stored in rev
-   
-	//this function will reverse the splited array
-    function reverse(split_element){
-        var revarr = [];
-        for(var i=split_element.length-1;i>=0;i--){
+{ //This function will check wether the elements in strarray is palindrome or not
+   let split_ele=n.split("");//splits the elements into an array
+    let rev = reverse(split_ele);//reversed element is stored in rev
+    
+	function reverse(split_element){//this function will reverse the splited array
+        let revarr = [];
+        for(let i=split_element.length-1;i>=0;i--){
             revarr.push(split_element[i]);
         }
         return revarr;
     }
-    var count = 0;
-    for(var k=0;k<rev.length;k++){
+    let count = 0;
+    for(let k=0;k<rev.length;k++){
         if(rev[k] === split_ele[k]){
             count++;
         }
@@ -52,13 +42,13 @@ function isPalindrome(n)
     }
 }
 
-for(var i=0;i<strarray.length;i++){
+for(let i=0;i<strarray.length;i++){
     if(isPalindrome(strarray[i])){
         console.log(strarray[i]);
-    }
+  	  }
+	}
 }
-}
-var array = [121,213,332,454,"ada",671,767,"sas"];
+let array = [121,213,445,454,"ada",671,767,"asa"];
 Palindrome(array);
 
 /*output : 
@@ -66,5 +56,5 @@ Palindrome(array);
 454
 ada
 767
-sas
+asa
 */
