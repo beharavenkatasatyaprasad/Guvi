8cc7040146d59aa18fbdb483ecdf2acb9cfa943b
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 11:58:53 +0530

    Update 11.js

--- day4/2.Warmup_Pbms/11.js
+++ day4/2.Warmup_Pbms/11.js
@@ -16,7 +16,7 @@ function CountAnimals(tur,horse,pigs) {
     //this function returns the total number of legs of all the animals
      try{
           if(typeof(tur) !== 'number' || typeof(horse) !== 'number' || typeof(pigs) !== 'number') throw 'Invalid input';
-        else console.log(2*tur+4*horse+4*pigs);;
+        else console.log(2*tur+4*horse+4*pigs);
     }catch(err){
         console.log(err);
     }
