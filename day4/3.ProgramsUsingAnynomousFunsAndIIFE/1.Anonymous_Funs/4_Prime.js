b6ac99a900bbe25237b322f00e34ad08f2a23916
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 12:32:28 +0530

    Update 4_Prime.js

--- day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/4_Prime.js
+++ day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/4_Prime.js
@@ -2,49 +2,33 @@
 Return all the prime numbers in an array
 */
 
-var Prime = function (array) { 
+let prime = function (array){ 
 
-for(var i=0;i<array.length;i++){
-    if (isPrime(array[i]))
- {
-	//prints the primes retured from isPrime function
+for(let i=0;i<array.length;i++){
+    if (isPrime(array[i])){//prints the primes retured from isPrime function
  try{
 		console.log(array[i]);
 	}catch(err){
-		console.log("Error occured")
+		console.log("Error has been occured");
 	}
  }
 } 
 //Returns true if a number is prime
-function isPrime(n)
-{   
-//this function will check wether the number is prime or not    
-  if (n===1)
-  {
+function isPrime(n){//this function will check wether the number is prime or not     
+   if (n===1){
     return false;
   }
-  else if(n === 2)
-  {
+  else if(n === 2){
     return true;
-  }else
-  {
-    for(var x = 2; x < n; x++)
-    {
-      if(n % x === 0)
-      {
+  }else{
+    for(let x = 2; x < n; x++){
+      if(n % x === 0){
         return false;
       }
     }
     return true;  
-  }
+ 		 }
+	}
 }
-}
-var array = [1,2,3,4,5,6,7,8,9];
-Prime(array)
-
-/*output : 
-2
-3
-5
-7
-*/
+let array = [1,8,7,5,6,3,7,6,3,7];
+prime(array);
