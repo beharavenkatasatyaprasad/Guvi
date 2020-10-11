2410a2aca2fce15fce50ec370a4baff6f3a29a53
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 12:47:18 +0530

    Update 30.js

--- day4/2.Warmup_Pbms/30.js
+++ day4/2.Warmup_Pbms/30.js
@@ -2,41 +2,27 @@
 Problem:
 Create a function that will return in an array the first “nPrimes” prime numbers greater than a particular number “startAt”
 */
-
-getPrimes(10, 100)
-function getPrimes(nPrimes, startAt)
-{
+function getPrimes(nPrimes, startAt){
  var i = startAt;
- n = 0;
- while(n < nPrimes)
- {
- if (isPrime(i))
- {
+ let n = 0;
+ while(n < nPrimes) {
+ if (isPrime(i)) {
  console.log(n, "→" , i);
  n++;
  }
- 
  i++;
  }
 }
 
-// Returns true if a number is prime
-function isPrime(n)
-{   
-    
-  if (n===1)
-  {
+function isPrime(n){// Returns true if a number is prime   
+  if (n===1){
     return false;
   }
-  else if(n === 2)
-  {
+  else if(n === 2){
     return true;
-  }else
-  {
-    for(var x = 2; x < n; x++)
-    {
-      if(n % x === 0)
-      {
+  }else{
+    for(let x = 2; x < n; x++){
+      if(n % x === 0){
         return false;
       }
     }
@@ -44,15 +30,25 @@ function isPrime(n)
   }
 }
 
-/*output:
+getPrimes(10, 100)
+/*o/p:
 0 '→' 101
+
 1 '→' 103
+
 2 '→' 107
+
 3 '→' 109
+
 4 '→' 113
+
 5 '→' 127
+
 6 '→' 131
+
 7 '→' 137
+
 8 '→' 139
+
 9 '→' 149
 */
