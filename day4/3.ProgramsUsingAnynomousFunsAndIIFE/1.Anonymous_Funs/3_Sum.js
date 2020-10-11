0a13d5be225bf11fc210dd55519f7c476f35637e
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 12:14:38 +0530

    Update 3_Sum.js

--- day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/3_Sum.js
+++ day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/3_Sum.js
@@ -1,20 +1,17 @@
 /*
 Sum of all numbers in an array
 */
-var Sum = function(array) { 
-	//This function will return sum of all the integers in an array
-	
-    var sum=0;
-    for(var i=0;i<array.length;i++){
+var sum = function(arr) { //This function will return sum of all the integers in an array
+    let sum=0;
+    for(let i=0;i<arr.length;i++){
         try{
-			
-			if(typeof(array[i]) !== 'number')throw 'There are NaN(s) in the given array at position(s) '+ i +" " +',but I can give the sum of remaining elements' 
+		if(typeof(arr[i]) !== 'number')throw 'There are NaN(s) in the given array at position(s) '+ i +" " +',but I can give the sum of remaining elements' 
 			
 		}catch(err){
 			console.log(err);
 		}finally{
-		    if(typeof(array[i]) === 'number')
-			sum = sum + array[i]
+		    if(typeof(arr[i]) === 'number')
+			sum = sum + arr[i]
 			
 		}
 		
@@ -22,13 +19,13 @@ var Sum = function(array) {
     return "sum: " + sum;
 }
 var array = [1,2,3,4,5,6,7,8,9]
-console.log(Sum(array));
+console.log(sum(array));
 
 /*output : 
 Sum: 45
 */
-var array2 =[1,2,3,4,"satya prasad",6,7,8,9]
-console.log(Sum(array2));
+var array2 =[1,2,3,4,"prasad",6,7,8,9]
+console.log(sum(array2));
 /*
 There are NaN(s) in the given array at positions 4 ,but I can give the sum of remaining elements
 sum: 40
