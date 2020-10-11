5ab17954bc39ccee42b37145ed601a4fce32849e
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 12:17:35 +0530

    Update 8_RotateArray.js

--- day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/8_RotateArray.js
+++ day4/3.ProgramsUsingAnynomousFunsAndIIFE/1.Anonymous_Funs/8_RotateArray.js
@@ -1,28 +1,23 @@
 /*
 Rotate an array by k times and return the rotated array
 */
-var RotateArray = function (array,ktimes) { 
+var RotateArray = function (array,k) { 
 
          function arrayRotate(arr) { 
             //This function will return the rotated array
             arr.unshift(arr.pop()); 
             return arr; 
         } 
-for(var i=0;i<ktimes;i++){
- //sends the input array to the function 'arrayRotate' ktimes
+for(let i=0;i<k;i++){//sends the input array to the function 'arrayRotate' ktimes
  array = arrayRotate(array);
 }
  try{
      console.log(array);
 }catch(err){
-    console.log("Error Occured")
+    console.log("Error has been Occured")
 }
 }
-array = [1, 2, 3, 4];
-ktimes = 3;
+array = [0, 1, 2, 3];
+ktimes = 8;
 RotateArray(array,ktimes)
 
-
-/*output : 
-[ 2, 3, 4, 1 ]
-*/
