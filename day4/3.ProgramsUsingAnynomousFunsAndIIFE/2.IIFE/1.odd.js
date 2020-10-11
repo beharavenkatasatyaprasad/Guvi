01616614e17e4fad481f509c24e9a882e1b08267
beharavenkatasatyaprasad <69835987+beharavenkatasatyaprasad@users.noreply.github.com>
2020-10-11 12:02:58 +0530

    Update 1.odd.js

--- day4/3.ProgramsUsingAnynomousFunsAndIIFE/2.IIFE/1.odd.js
+++ day4/3.ProgramsUsingAnynomousFunsAndIIFE/2.IIFE/1.odd.js
@@ -1,16 +1,18 @@
 /*
 Problem:Print odd numbers in an array 
 */
-(function(array) { 
+(function (array) { 
     var oddarray=[];
     for(var i=0;i<array.length;i++){
-        if(array[i]%2!=0){
+        if(array[i]%2!==0){
             try{
 				console.log(array[i]);
 			}catch(err){
 			console.log("Error Occured");
             }
         }
+    }
+    
 })(array = [1,2,3,4,5,6,7,8,9]);
 
 /*output : 
